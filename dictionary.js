658b012fecae00967d23457ca6d26bb60b94c859
Tanis Pan <e.pan.gar@gmail.com>
2019-02-15 00:21:09 +0100

    dictionary

---
+++ dictionary.js
@@ -0,0 +1,12 @@
+String.prototype.dictionary=function(){
+	var answer={};
+
+	this.split("").forEach(function(character){
+		if(answer[character]){
+			answer[character]++;
+		} else {
+			answer[character]=1;
+		}
+	});
+	return answer;
+};
