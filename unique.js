d4125481f89738070b3ce0acc8192fdcf888b6d2
Tanis Pan <e.pan.gar@gmail.com>
2019-02-14 22:00:05 +0100

    unique.js

--- unique.js
+++ unique.js
@@ -1,1 +1,11 @@
-//
+Array.prototype.unique = function() {
+	var arr = this, answer = [];
+
+	arr.forEach(function(element) {
+		if (!answer.includes(element)) {
+			answer.push(element);
+		}
+	});
+
+	return answer;
+};
