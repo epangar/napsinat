f3a639b91b2d0cd1df34c7a3f25f930d671c1d52
Estanislao Pan García <epan@indra.es>
2019-02-15 15:24:27 +0100

    tests

---
+++ test/dictionaryTest.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+var shouls = require("chai").should();
+var unique = require("../Methods/String/dictionary");
+
+describe("Test the behavoiur of dictionary()", function(){
+    it("should return ")
+
+});
