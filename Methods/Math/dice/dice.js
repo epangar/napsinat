87ad5cc863e4a4abf7d547ea6b2208f190e84ed9
Tanis Pan <e.pan.gar@gmail.com>
2019-02-17 16:27:13 +0100

    dice

---
+++ Methods/Math/dice/dice.js
@@ -0,0 +1,10 @@
+Math.dice=function(number, sides){
+  var arr=[]
+
+  for(i=0;i<number;i++){
+    var roll=Math.floor(Math.random()*sides+1)
+    arr.push(roll)
+  }
+  var result=arr.reduce(function(a,b){return a+b})
+  return [result, arr]
+}
