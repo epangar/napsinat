20c5a547a5c6468491122f1b6454b8d7330affca
Estanislao Pan García <epan@indra.es>
2019-02-18 12:30:51 +0100

    comments

--- Methods/Math/circlePeri/circlePeri.js
+++ Methods/Math/circlePeri/circlePeri.js
@@ -1,3 +1,5 @@
+//Returns the perimeter of a circle with a given radius
+
 Math.circlePeri=function(radius){
     var num = Math.PI*2*radius
     return parseFloat(num.toFixed(2))
--- Methods/String/first/first.js
+++ Methods/String/first/first.js
@@ -1,3 +1,5 @@
+//Returns the first element of a string
+
 String.prototype.first=function(){
   return this[0];
 }
