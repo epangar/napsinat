9092f8cf0a2fd2b7100ad3e28f9c2220cba3bff9
Estanislao Pan García <epan@indra.es>
2019-07-22 12:16:08 +0200

    Jargon Class

--- src/Classes/Jargon/Jargon.js
+++ src/Classes/Jargon/Jargon.js
@@ -4,7 +4,18 @@ class Jargon {
     this.duration=duration;
   }
   
+  content(){
+    var answer="";
+
+    for(var i=0;i<this.duration;i++){
+      var char=Math.floor(Math.random()*94)+33;
+      answer+=String.fromCharCode(char);
+    }
+
+    return answer;
+  }
 }
 
-var x=new Jargon(5)
+var x=new Jargon(55)
 console.log(x)
+console.log(x.content())
