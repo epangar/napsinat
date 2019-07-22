9ab622f5536570c9de8b7c47e7f1a8de15b63d84
Estanislao Pan García <epan@indra.es>
2019-07-22 11:00:00 +0200

    Classes

--- src/Class/Jargon/Jargon.js
+++
@@ -1,8 +0,0 @@
-function Jargon(n) {
-  this.n=n;
-  return this.n
-  
-}
-
-var x=new Jargon(5)
-console.log(x)
---
+++ src/Classes/Jargon/Jargon.js
@@ -0,0 +1,10 @@
+class Jargon {
+  
+  constructor(duration){
+    this.duration=duration;
+  }
+  
+}
+
+var x=new Jargon(5)
+console.log(x)
