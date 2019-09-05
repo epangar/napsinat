79ec83c5237d273035cf23da09f9063a179e3a34
Estanislao Pan García <epan@indra.es>
2019-09-05 10:50:08 +0200

    Refactorización

--- src/Methods/Math/rangeCube/rangeCube.js
+++ src/Methods/Math/rangeCube/rangeCube.js
@@ -1,11 +1,17 @@
 //Returns an array of the nth first cube numbers
 
-Math.rangeCube=function(n){
-  var answer=[];
+// Math.rangeCube=function(n){
+//   var answer=[];
 
-  for(i=0;i<n;i++){
-    answer.push(Math.pow(i,3))
-  }
+//   for(i=0;i<n;i++){
+//     answer.push(Math.pow(i,3))
+//   }
  
-  return answer;
-}
+//   return answer;
+// }
+
+Math.rangeCube= (n=10) => new Array(n).fill()
+                      .map((element, position) => Math.pow(position,3)
+                      )
+
+
