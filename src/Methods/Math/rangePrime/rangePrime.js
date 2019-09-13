8e0ade03f62086c9ed2ad70d876e6ae35ee61974
Estanislao Pan García <epan@indra.es>
2019-09-13 12:41:49 +0200

    Array.pos

--- src/Methods/Math/rangePrime/rangePrime.js
+++ src/Methods/Math/rangePrime/rangePrime.js
@@ -3,7 +3,7 @@
 var isPrime = require("../isPrime/isPrime");
 var range = require("../range/range");
 
-Math.rangePrime=function(start,end){
+Math.rangePrime=function(start=0,end=30){
   return Math.range(start,end).filter(function(number){
                                   if(Math.isPrime(number)){
                                     return number;
